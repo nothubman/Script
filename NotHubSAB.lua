9513051a423549d7fe0eb821dcd77254da8bf6b5
nothubman <manthanchokshi43@gmail.com>
2026-02-17 18:08:09 +0530

    Create NotHubSAB.lua

---
+++ NotHubSAB.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY='KEYLESS'
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/4a5b8f7cf46fd61ba01435e8f7375378df072db1569c65ecd0a3904a2bb0a0de/download"))()
